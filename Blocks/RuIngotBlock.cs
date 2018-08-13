385e1aefa09af943ca3297836a77e08223343aa2
freeze <1347360374@qq.com>
2018-08-13 15:26:04 +0800

    optimize

--- Blocks/RuIngotBlock.cs
+++ Blocks/RuIngotBlock.cs
@@ -4,17 +4,14 @@ using Engine.Graphics;
 
 namespace Game
 {
-	// Token: 0x0200061C RID: 1564
 	public class RuIngotBlock : CubeBlock
 	{
-		// Token: 0x060021A9 RID: 8617
 		public RuIngotBlock()
 		{
 			this.m_standaloneBlockMesh = new BlockMesh();
 			this.m_standaloneMesh = new BlockMesh();
 		}
 
-		// Token: 0x060021AA RID: 8618
 		public override void Initialize()
 		{
 			Model model = ContentManager.Get<Model>("Models/Campfire");
@@ -23,19 +20,15 @@ namespace Game
 			base.Initialize();
 		}
 
-		// Token: 0x060021AB RID: 8619
 		public override void DrawBlock(PrimitivesRenderer3D primitivesRenderer, int value, Color color, float size, ref Matrix matrix, DrawBlockEnvironmentData environmentData)
 		{
 			BlocksManager.DrawMeshBlock(primitivesRenderer, this.m_standaloneMesh, color, size * 2f, ref matrix, environmentData);
 		}
 
-		// Token: 0x040019AD RID: 6573
 		public const int Index = 546;
 
-		// Token: 0x040019AE RID: 6574
 		private readonly BlockMesh m_standaloneBlockMesh;
 
-		// Token: 0x040019B2 RID: 6578
 		private readonly BlockMesh m_standaloneMesh;
 	}
 }
