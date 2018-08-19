1f22dfa98e298056057932d717f90dbe6887225a
FFF <0@ccdd.ga>
2018-08-19 11:52:46 +0800

    矿石可投掷

---
+++ Subsystems/SubsystemItemBlockBehavior.cs
@@ -0,0 +1,18 @@
+﻿using Engine;
+namespace Game
+{
+	public class SubsystemItemBlockBehavior : SubsystemThrowableBlockBehavior
+	{
+		public override int[] HandledBlocks => new int[] { ItemBlock.Index };
+
+		public override bool OnAim(Vector3 start, Vector3 direction, ComponentMiner componentMiner, AimState state)
+		{
+			int value = componentMiner.ActiveBlockValue;
+			if (!(BlocksManager.Blocks[Terrain.ExtractContents(value)] is ItemBlock item))
+				return false;
+			if (item.GetItem(ref value) is OreChunkBlock)
+				return base.OnAim(start, direction, componentMiner, state);
+			return false;
+		}
+	}
+}
