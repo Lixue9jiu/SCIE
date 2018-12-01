61dcbd41bad3190b283920768231b005fcfc898b
FFF <0@ccdd.ga>
2018-12-01 20:24:28 +0800

    修复

--- Subsystems/SubsystemDiversionBlockBehavior.cs
+++ Subsystems/SubsystemDiversionBlockBehavior.cs
@@ -12,8 +12,6 @@ namespace Game
 			{
 				return;
 			}
-			//CellFace.FaceToVector3(cellFace.Face);
-			//CellFace.FaceToVector3(FourDirectionalBlock.GetDirection(SubsystemTerrain.Terrain.GetCellValue(cellFace.X, cellFace.Y, cellFace.Z)));
 			Vector3 v = CellFace.FaceToVector3((SubsystemTerrain.Terrain.GetCellValue(cellFace.X, cellFace.Y, cellFace.Z) - DiversionBlock.Index) >> 14);
 			Utils.SubsystemProjectiles.FireProjectile(worldItem.Value, new Vector3((float)cellFace.X + 0.5f, (float)cellFace.Y + 0.5f, (float)cellFace.Z + 0.5f) + 0.75f * v, 30f * v, Vector3.Zero, null);
 			worldItem.ToRemove = true;
