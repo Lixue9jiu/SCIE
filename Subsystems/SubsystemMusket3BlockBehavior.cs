43b0f6a7ee19b6f86c422ac3f9f9ff7295e53310
p_dem <p_dembowski@163.com>
2019-08-02 10:58:19 +0800

    musket3

--- Subsystems/SubsystemMusket3BlockBehavior.cs
+++ Subsystems/SubsystemMusket3BlockBehavior.cs
@@ -40,16 +40,13 @@ namespace Game
 							Z = SimplexNoise.OctavedNoise(num5 + 200f, 2f, 3, 2f, 0.5f)
 						};
 						direction = Vector3.Normalize(direction + v);
+                        int n = 1;
 						switch (state)
 						{
 						case AimState.InProgress:
 						{
-							if (num4 >= 30f)
-							{
-								componentMiner.ComponentCreature.ComponentCreatureSounds.PlayMoanSound();
-								return true;
-							}
-							if (num4 > 0.2f && !Musket3Block.GetHammerState(Terrain.ExtractData(num2)))
+							
+							if (num4 > 0.3f && !Musket3Block.GetHammerState(Terrain.ExtractData(num2)))
 							{
 								num2 = Terrain.MakeBlockValue(num, 0, Musket3Block.SetHammerState(Terrain.ExtractData(num2), true));
 								m_subsystemAudio.PlaySound("Audio/HammerCock", 1f, m_random.UniformFloat(-0.1f, 0.1f), 0f, 0f);
@@ -63,9 +60,43 @@ namespace Game
 								componentFirstPersonModel.ItemOffsetOrder = new Vector3(-0.21f, 0.15f, 0.08f);
 								componentFirstPersonModel.ItemRotationOrder = new Vector3(-0.7f, 0f, 0f);
 							}
-							componentMiner.ComponentCreature.ComponentCreatureModel.AimHandAngleOrder = 1.4f;
-							componentMiner.ComponentCreature.ComponentCreatureModel.InHandItemOffsetOrder = new Vector3(-0.08f, -0.08f, 0.07f);
-							componentMiner.ComponentCreature.ComponentCreatureModel.InHandItemRotationOrder = new Vector3(-1.7f, 0f, 0f);
+                                    if (m_subsystemTime.PeriodicGameTimeEvent(0.15, 0))
+                                    {
+                                        n++;
+                                        Vector3 vector = Vector3.Zero;
+                                        if (componentMiner.ComponentCreature.ComponentBody.ImmersionFactor > 0.4f)
+                                            m_subsystemAudio.PlaySound("Audio/MusketMisfire", 1f, m_random.UniformFloat(-0.1f, 0.1f), componentMiner.ComponentCreature.ComponentCreatureModel.EyePosition, 3f, true);
+                                        else
+                                        {
+                                            Vector3 eyePosition = componentMiner.ComponentCreature.ComponentCreatureModel.EyePosition;
+                                            Matrix matrix = componentMiner.ComponentCreature.ComponentBody.Matrix;
+                                            Vector3 v2 = eyePosition + matrix.Right * 0.3f;
+                                            matrix = componentMiner.ComponentCreature.ComponentBody.Matrix;
+                                            Vector3 vector2 = v2 - matrix.Up * 0.2f;
+                                            var vector3 = Vector3.Normalize(vector2 + direction * 10f - vector2);
+                                            var vector4 = Vector3.Normalize(Vector3.Cross(vector3, Vector3.UnitY));
+                                            var v3 = Vector3.Normalize(Vector3.Cross(vector3, vector4));
+                                            int num6 = 1;
+                                            vector = new Vector3(0.01f, 0.01f, 0.05f);
+                                            for (int i = 0; i < num6; i++)
+                                            {
+                                                int value2 = Terrain.MakeBlockValue(214, 0, BulletBlock.SetBulletType(0, BulletBlock.BulletType.IronBullet));
+                                                Vector3 v4 = m_random.UniformFloat(0f - vector.X, vector.X) * vector4 + m_random.UniformFloat(0f - vector.Y, vector.Y) * v3 + m_random.UniformFloat(0f - vector.Z, vector.Z) * vector3;
+                                                Projectile projectile = m_subsystemProjectiles.FireProjectile(value2, vector2, 320f * (vector3 + v4), Vector3.Zero, componentMiner.ComponentCreature);
+                                                if (projectile != null)
+                                                    projectile.ProjectileStoppedAction = ProjectileStoppedAction.Disappear;
+                                            }
+                                            m_subsystemAudio.PlaySound("Audio/MusketFire", 1f, m_random.UniformFloat(-0.1f, 0.1f), componentMiner.ComponentCreature.ComponentCreatureModel.EyePosition, 10f, true);
+                                            // m_subsystemParticles.AddParticleSystem(new GunSmokeParticleSystem(SubsystemTerrain, vector2 + 0.3f * vector3, vector3));
+                                            m_subsystemNoise.MakeNoise(vector2, 1f, 40f);
+                                          
+                                            // componentMiner.ComponentCreature.ComponentBody.ApplyImpulse(-1f * vector3);
+                                        }
+                                    } 
+                                        componentMiner.ComponentCreature.ComponentCreatureModel.AimHandAngleOrder = 1.4f;
+                                        componentMiner.ComponentCreature.ComponentCreatureModel.InHandItemOffsetOrder = new Vector3(-0.08f, -0.08f, 0.07f);
+                                        componentMiner.ComponentCreature.ComponentCreatureModel.InHandItemRotationOrder = new Vector3(-1.7f, 0f, 0f);
+                                    
 							break;
 						}
 						case AimState.Cancelled:
@@ -78,81 +109,13 @@ namespace Game
 							break;
 						case AimState.Completed:
 						{
-							bool flag = false;
-							int value2 = 0;
-							int num6 = 0;
-							float s = 0f;
-							Vector3 vector = Vector3.Zero;
-							Musket3Block.LoadState loadState = Musket3Block.GetLoadState(data);
-							if (Musket3Block.GetHammerState(Terrain.ExtractData(num2)))
-							{
-								switch (loadState)
-								{
-								case Musket3Block.LoadState.bullet:
-									flag = true;
-									value2 = Terrain.MakeBlockValue(214, 0, BulletBlock.SetBulletType(0, BulletBlock.BulletType.IronBullet));
-									s = 320f;
-									num6 = 1;
-									vector = new Vector3(0.01f, 0.01f, 0.05f);
-									goto default;
-								default:
-									if (loadState == Musket3Block.LoadState.bullet2)
-									{
-										flag = true;
-										value2 = Terrain.MakeBlockValue(214, 0, BulletBlock.SetBulletType(0, BulletBlock.BulletType.IronBullet));
-										s = 320f;
-										num6 = 1;
-										vector = new Vector3(0.01f, 0.01f, 0.05f);
-									}
-									break;
-								case Musket3Block.LoadState.Empty:
-								{
-									ComponentPlayer componentPlayer = componentMiner.ComponentPlayer;
-									if (componentPlayer != null)
-										componentPlayer.ComponentGui.DisplaySmallMessage("Load bullet first", true, false);
-									break;
-								}
-								}
-							}
-							if (flag)
-							{
-								if (componentMiner.ComponentCreature.ComponentBody.ImmersionFactor > 0.4f)
-									m_subsystemAudio.PlaySound("Audio/MusketMisfire", 1f, m_random.UniformFloat(-0.1f, 0.1f), componentMiner.ComponentCreature.ComponentCreatureModel.EyePosition, 3f, true);
-								else
-								{
-									Vector3 eyePosition = componentMiner.ComponentCreature.ComponentCreatureModel.EyePosition;
-									Matrix matrix = componentMiner.ComponentCreature.ComponentBody.Matrix;
-									Vector3 v2 = eyePosition + matrix.Right * 0.3f;
-									matrix = componentMiner.ComponentCreature.ComponentBody.Matrix;
-									Vector3 vector2 = v2 - matrix.Up * 0.2f;
-									var vector3 = Vector3.Normalize(vector2 + direction * 10f - vector2);
-									var vector4 = Vector3.Normalize(Vector3.Cross(vector3, Vector3.UnitY));
-									var v3 = Vector3.Normalize(Vector3.Cross(vector3, vector4));
-									for (int i = 0; i < num6; i++)
-									{
-										Vector3 v4 = m_random.UniformFloat(0f - vector.X, vector.X) * vector4 + m_random.UniformFloat(0f - vector.Y, vector.Y) * v3 + m_random.UniformFloat(0f - vector.Z, vector.Z) * vector3;
-										Projectile projectile = m_subsystemProjectiles.FireProjectile(value2, vector2, s * (vector3 + v4), Vector3.Zero, componentMiner.ComponentCreature);
-										if (projectile != null)
-											projectile.ProjectileStoppedAction = ProjectileStoppedAction.Disappear;
-									}
-									m_subsystemAudio.PlaySound("Audio/MusketFire", 1f, m_random.UniformFloat(-0.1f, 0.1f), componentMiner.ComponentCreature.ComponentCreatureModel.EyePosition, 10f, true);
-									m_subsystemParticles.AddParticleSystem(new GunSmokeParticleSystem(SubsystemTerrain, vector2 + 0.3f * vector3, vector3));
-									m_subsystemNoise.MakeNoise(vector2, 1f, 40f);
-									componentMiner.ComponentCreature.ComponentBody.ApplyImpulse(-1f * vector3);
-								}
-								if (loadState == Musket3Block.LoadState.bullet)
-									num2 = Terrain.MakeBlockValue(Terrain.ExtractContents(num2), 0, Musket3Block.SetLoadState(Terrain.ExtractData(num2), Musket3Block.LoadState.Empty));
-								if (loadState == Musket3Block.LoadState.bullet2)
-									num2 = Terrain.MakeBlockValue(Terrain.ExtractContents(num2), 0, Musket3Block.SetLoadState(Terrain.ExtractData(num2), Musket3Block.LoadState.bullet));
-								num3 = 1;
-							}
-							if (Musket3Block.GetHammerState(Terrain.ExtractData(num2)))
-							{
-								num2 = Terrain.MakeBlockValue(Terrain.ExtractContents(num2), 0, Musket3Block.SetHammerState(Terrain.ExtractData(num2), false));
-								m_subsystemAudio.PlaySound("Audio/HammerRelease", 1f, m_random.UniformFloat(-0.1f, 0.1f), 0f, 0f);
-							}
-							m_aimStartTimes.Remove(componentMiner);
-							break;
+                                    if (Musket3Block.GetHammerState(Terrain.ExtractData(num2)))
+                                    {
+                                        num2 = Terrain.MakeBlockValue(num, 0, Musket3Block.SetHammerState(Terrain.ExtractData(num2), false));
+                                        m_subsystemAudio.PlaySound("Audio/HammerUncock", 1f, m_random.UniformFloat(-0.1f, 0.1f), 0f, 0f);
+                                    }
+                                    m_aimStartTimes.Remove(componentMiner);
+                                    break;
 						}
 						}
 					}
