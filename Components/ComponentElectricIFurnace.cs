3f8c1a0f334912c97fed76aa241c785c81d4311f
p_dem <p_dembowski@163.com>
2019-08-03 21:11:58 +0800

    123

--- Components/ComponentElectricIFurnace.cs
+++ Components/ComponentElectricIFurnace.cs
@@ -36,7 +36,7 @@ namespace Game
 			{
 				m_updateSmeltingRecipe = false;
 				//float heatLevel = HeatLevel > 0f ? HeatLevel : 2000f;
-				CraftingRecipe craftingRecipe = FindSmeltingRecipe(3000f);
+				CraftingRecipe craftingRecipe = FindSmeltingRecipe1(3000f);
 				if (craftingRecipe != m_smeltingRecipe)
 				{
 					m_smeltingRecipe = craftingRecipe;
@@ -117,5 +117,60 @@ namespace Game
 			this.SaveItems(valuesDictionary);
 			valuesDictionary.SetValue("FireTimeRemaining", m_fireTimeRemaining);
 		}
-	}
+        public CraftingRecipe FindSmeltingRecipe1(float heatLevel)
+        {
+            if (heatLevel >= 0f)
+            {
+                for (int i = 0; i < m_furnaceSize; i++)
+                {
+                    int slotValue = GetSlotValue(i);
+                    int num = Terrain.ExtractContents(slotValue);
+                    int num2 = Terrain.ExtractData(slotValue);
+                    if (GetSlotCount(i) > 0)
+                    {
+                        Block block = BlocksManager.Blocks[num];
+                        m_matchedIngredients[i] = block.CraftingId + ":" + num2.ToString(CultureInfo.InvariantCulture);
+                    }
+                    else
+                    {
+                        m_matchedIngredients[i] = null;
+                    }
+                }
+                CraftingRecipe craftingRecipe = CraftingRecipesManager.FindMatchingRecipe(m_subsystemTerrain, m_matchedIngredients, heatLevel);
+                if (craftingRecipe != null)
+                {
+                    if (craftingRecipe.RequiredHeatLevel != 2800f)
+                    {
+                        craftingRecipe = null;
+                    }
+                    if (craftingRecipe != null)
+                    {
+                        Slot slot = m_slots[ResultSlotIndex];
+                        int num3 = Terrain.ExtractContents(craftingRecipe.ResultValue);
+                        if (slot.Count != 0 && (craftingRecipe.ResultValue != slot.Value || craftingRecipe.ResultCount + slot.Count > BlocksManager.Blocks[num3].MaxStacking))
+                        {
+                            craftingRecipe = null;
+                        }
+                    }
+                    if (craftingRecipe != null && craftingRecipe.RemainsValue != 0 && craftingRecipe.RemainsCount > 0)
+                    {
+                        if (m_slots[RemainsSlotIndex].Count == 0 || m_slots[RemainsSlotIndex].Value == craftingRecipe.RemainsValue)
+                        {
+                            if (BlocksManager.Blocks[Terrain.ExtractContents(craftingRecipe.RemainsValue)].MaxStacking - m_slots[RemainsSlotIndex].Count < craftingRecipe.RemainsCount)
+                            {
+                                craftingRecipe = null;
+                            }
+                        }
+                        else
+                        {
+                            craftingRecipe = null;
+                        }
+                    }
+                }
+                return craftingRecipe;
+            }
+            return null;
+        }
+
+    }
 }
