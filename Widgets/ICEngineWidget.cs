6b1ece67f4223750f3193ef9613291b86b4ee148
p_dem <p_dembowski@163.com>
2019-08-10 20:50:08 +0800

    12

--- Widgets/ICEngineWidget.cs
+++ Widgets/ICEngineWidget.cs
@@ -75,28 +75,20 @@ namespace Game
 
 		public override void Update()
 		{
-			//Children.Find<LabelWidget>("DispenserLabel2").Text = (m_componentDispenser2.m_fireTimeRemaining).ToString() + "/10M E";
-			//int value = m_subsystemTerrain.Terrain.GetCellValue(m_componentBlockEntity.Coordinates.X, m_componentBlockEntity.Coordinates.Y, m_componentBlockEntity.Coordinates.Z);
-			//int data = Terrain.ExtractData(value);
-			//MachineMode1 mode = GetMode(data);
-			//if (m_dispenseButton.IsClicked && mode == MachineMode1.Discharger)
-			//{
-			//	data = SetMode(data);
-			//
-			//	m_subsystemTerrain.Terrain.SetCellValueFast(m_componentBlockEntity.Coordinates.X, m_componentBlockEntity.Coordinates.Y, m_componentBlockEntity.Coordinates.Z, Terrain.ReplaceData(value, data));
-			//}
-			//if (m_shootButton.IsClicked && mode == MachineMode1.Charge)
-			//{
-			//	data = SetMode(data);
-			//	m_subsystemTerrain.Terrain.SetCellValueFast(m_componentBlockEntity.Coordinates.X, m_componentBlockEntity.Coordinates.Y, m_componentBlockEntity.Coordinates.Z, Terrain.ReplaceData(value, data));
-			//	m_componentDispenser2.Charged = false;
-			//}
 			m_fire.ParticlesPerSecond = m_componentDispenser2.HeatLevel > 0f ? 24f : 0f;
-			m_progress.Value = m_componentDispenser2.m_fireTimeRemaining / 1000f;
-			//m_dispenseButton.IsChecked = mode == MachineMode1.Charge;
-			//m_componentDispenser2.Charged = mode == MachineMode1.Charge;
-			//m_shootButton.IsChecked = mode == MachineMode1.Discharger;
-			//m_acceptsDropsBox.IsChecked = SixDirectionalBlock.GetAcceptsDrops(data);
+			m_progress.Value = m_componentDispenser2.SmeltingProgress / 1000f;
+
+
+			if (m_dispenseButton.IsClicked && m_componentDispenser2.HeatLevel <= 0f && m_componentDispenser2.SmeltingProgress >= 0f)
+			{
+				m_componentDispenser2.HeatLevel = 1000f;
+			}
+			if (m_shootButton.IsClicked && m_componentDispenser2.HeatLevel > 0f)
+			{
+				m_componentDispenser2.HeatLevel = 0f;
+			}
+			m_dispenseButton.IsChecked = m_componentDispenser2.HeatLevel != 0f;
+			
 			if (!m_componentDispenser2.IsAddedToProject)
 			{
 				ParentWidget.Children.Remove(this);
