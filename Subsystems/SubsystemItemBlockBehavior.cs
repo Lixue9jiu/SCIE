5362b18a558220889e7e5ad1517a7ae53346ee48
p_dem <p_dembowski@163.com>
2019-08-11 14:28:59 +0800

    car

--- Subsystems/SubsystemItemBlockBehavior.cs
+++ Subsystems/SubsystemItemBlockBehavior.cs
@@ -263,6 +263,12 @@ namespace Game
 					entity.FindComponent<ComponentFrame>(true).Rotation = Quaternion.CreateFromAxisAngle(Vector3.UnitY, m_random.UniformFloat(0f, 6.283185f));
 					goto put;
 				}
+				else if (activeBlockValue == ItemBlock.IdTable["Car"])
+				{
+					entity = DatabaseManager.CreateEntity(Project, "Car", true);
+					entity.FindComponent<ComponentFrame>(true).Rotation = Quaternion.CreateFromAxisAngle(Vector3.UnitY, m_random.UniformFloat(0f, 6.283185f));
+					goto put;
+				}
 				else if (BlocksManager.Blocks[Terrain.ExtractContents(activeBlockValue)] is ItemBlock item && item.GetItem(ref activeBlockValue) is Mine mine)
 				{
 					entity = DatabaseManager.CreateEntity(Project, "Mine", new ValuesDictionary
