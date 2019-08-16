1f040743190e0e424273e2cb530d988ff4de6c41
p_dem <p_dembowski@163.com>
2019-08-16 12:42:51 +0800

    123

--- Components/ComponentElectricDriller.cs
+++ Components/ComponentElectricDriller.cs
@@ -16,7 +16,7 @@ namespace Game
 
 		public void Update(float dt)
 		{
-			if (Charged)
+			if (Charged && Powered)
 			{
 				m_fireTimeRemaining = MathUtils.Max(0f, m_fireTimeRemaining - dt);
 				if (m_fireTimeRemaining<1f)
@@ -34,23 +34,26 @@ namespace Game
 			this.LoadItems(valuesDictionary);
 			m_componentBlockEntity = Entity.FindComponent<ComponentBlockEntity>(true);
 			m_fireTimeRemaining = valuesDictionary.GetValue("FireTimeRemaining", 0f);
-			Charged = valuesDictionary.GetValue("HeatLevel", 0f) == 0f ? true : false;
+			Charged = valuesDictionary.GetValue("HeatLevel", 0f) != 0f ? true : false;
 		}
 
 		public override void Save(ValuesDictionary valuesDictionary, EntityToIdMap entityToIdMap)
 		{
 			this.SaveItems(valuesDictionary);
 			valuesDictionary.SetValue("FireTimeRemaining", m_fireTimeRemaining);
-			valuesDictionary.SetValue("HeatLevel", Charged ? 0f : 1f);
+			valuesDictionary.SetValue("HeatLevel", Charged ? 1f : 0f);
 		}
 
 
 		protected void Driller(Point3 point)
 		{
 			int value = GetSlotValue(8);
+			int value2 = GetSlotValue(9);
+			int value3 = GetSlotCount(9);
 			if (BlocksManager.Blocks[Terrain.ExtractContents(value)].Durability <= 0)
 				return;
 			int a = 40;
+			bool flag=false;
 			for (int x1 = -a+point.X; x1 <  a + 1 + point.X; x1++)
 			{
 				for (int y1 = 0; y1 < point.Y; y1++)
@@ -59,7 +62,35 @@ namespace Game
 					{
 						int cellValue = Utils.Terrain.GetCellValueFast(x1, y1, z1);
 						var block = BlocksManager.Blocks[Terrain.ExtractContents(cellValue)];
-						if (block.BlockIndex == BasaltBlock.Index && cellValue!= BasaltBlock.Index && cellValue !=536870979 && cellValue !=1073741891 && cellValue != 1610612803)
+						if (value2!=0  && value3>0)
+						{
+							flag = true;
+							if (cellValue != value2)
+							{
+								flag = true;
+							}
+							else
+							{
+								ComponentElectricDriller inventory = m_componentBlockEntity.Entity.FindComponent<ComponentElectricDriller>(throwOnError: true);
+								if (AcquireItems(inventory, cellValue, 1) != 0)
+								{
+									Charged = false;
+									return;
+								}
+								Utils.SubsystemTerrain.ChangeCell(x1, y1, z1, 0, true);
+								DrillType type = DrillBlock.GetType(value);
+								RemoveSlotItems(8, 1);
+								if (DrillBlock.GetType(BlocksManager.DamageItem(value, 1)) == type && BlocksManager.DamageItem(value, 1) != value && GetDamage(BlocksManager.DamageItem(value, 1)) != GetDamage(value))
+								{
+									AddSlotItems(8, BlocksManager.DamageItem(value, 1), 1);
+								}
+								return;
+							}
+						}
+
+
+
+						if (block.BlockIndex == BasaltBlock.Index && cellValue!= BasaltBlock.Index && cellValue !=536870979 && cellValue !=1073741891 && cellValue != 1610612803 && !flag)
 						{
 							
 							ComponentElectricDriller inventory = m_componentBlockEntity.Entity.FindComponent<ComponentElectricDriller>(throwOnError: true);
@@ -78,7 +109,7 @@ namespace Game
 							}
 							return;
 						}
-						if (block.BlockIndex==IronOreBlock.Index|| block.BlockIndex==CopperOreBlock.Index|| block.BlockIndex == DiamondOreBlock.Index || block.BlockIndex==GermaniumOreBlock.Index || block.BlockIndex ==SaltpeterOreBlock.Index || block.BlockIndex==SulphurOreBlock.Index|| block.BlockIndex==CoalOreBlock.Index)
+						if ((block.BlockIndex==IronOreBlock.Index|| block.BlockIndex==CopperOreBlock.Index|| block.BlockIndex == DiamondOreBlock.Index || block.BlockIndex==GermaniumOreBlock.Index || block.BlockIndex ==SaltpeterOreBlock.Index || block.BlockIndex==SulphurOreBlock.Index|| block.BlockIndex==CoalOreBlock.Index) && !flag)
 						{
 							
 							ComponentElectricDriller inventory = m_componentBlockEntity.Entity.FindComponent<ComponentElectricDriller>(throwOnError: true);
@@ -99,6 +130,9 @@ namespace Game
 					}
 				}
 			}
+			Charged = false;
+			return;
+
 		}
 		public virtual int GetDamage(int value)
 		{
@@ -106,17 +140,12 @@ namespace Game
 		}
 		public override int GetSlotCapacity(int slotIndex, int value)
 		{
-			if (slotIndex != 8 && slotIndex != 9)
+			if (slotIndex != 8)
 				return base.GetSlotCapacity(slotIndex, value);
 			if (slotIndex == 8 && Terrain.ExtractContents(value) == DrillBlock.Index)
 			{
 				var type = DrillBlock.GetType(value);
 				return (type == DrillType.DiamondDrill || type == DrillType.SteelDrill) ? base.GetSlotCapacity(slotIndex, value) : 0;
-			}
-			if (slotIndex == 9 && Terrain.ExtractContents(value) == IEBatteryBlock.Index)
-			{
-				var type = IEBatteryBlock.GetType(value);
-				return (type == BatteryType.Cu_Zn_Battery || type == BatteryType.Lead_Battery || type == BatteryType.Fission_Battery || type == BatteryType.Fusion_Battery) ? base.GetSlotCapacity(slotIndex, value) : 0;
 			}
 			return 0;
 
